79979c08d8e4706bcc27b9667fa1241b617235da
Yogi_Patel <yogiranchhod@gmail.com>
2023-09-18 02:24:05 -0700

    Added yogipatel.dev to Allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yogipatel-dev-4094d9dbd286.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['yogipatel-dev-4094d9dbd286.herokuapp.com', '127.0.0.1', 'yogipatel.dev']
 
 
 # Application definition
