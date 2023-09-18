f2ed85dedd96a60a260f2897cec277667ec3f69a
Yogi_Patel <yogiranchhod@gmail.com>
2023-09-17 18:49:02 -0700

    Added support for Media Files
    Due the ephemeral nature of Heroku's file system, there is no support for media files. Therefore, to add support I have added cloudinary to the project

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-#import sensitiveInformation
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv("SECRET_KEY")#sensitiveInformation.SECRET_KEY # SECRET_KEY stored in sensitiveInformation.py and that file is in gitignore to avoid pushing it to the remote branch
+SECRET_KEY =  os.getenv('SECRET_KEY')#sensitiveInformation.SECRET_KEY # SECRET_KEY stored in sensitiveInformation.py and that file is in gitignore to avoid pushing it to the remote branch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -42,11 +41,13 @@ INSTALLED_APPS = [
 
 
     # Local Apps
-    "website",
+    'website',
 
 
     #3rd party apps
-    "django_cleanup.apps.CleanupConfig", # automatically deletes images when their respective objects are deleted from the model    
+    'django_cleanup.apps.CleanupConfig', # automatically deletes images when their respective objects are deleted from the model    
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -166,11 +167,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 
 # Settings for sending emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')   
